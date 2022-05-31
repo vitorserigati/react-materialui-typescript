d7a93240a9a67d7dc77fd8cec48a2310ca3c5c08
Vitor Serigati <vitor_serigati@hotmail.com>
2022-05-31 04:15:56 -0300

    feat: Adição do Material UI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 import logo from '../logo.svg'
 
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/home" element={ <img src={logo}/>} />
+            <Route path="/home" element={  <img src={logo}/>} />
             
             <Route path="*" element={<Navigate to="/home"/>}/>
         </Routes>
